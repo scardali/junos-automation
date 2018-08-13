f8a6a1f588ff1165d389a5668006eeb41c1ffe85
Sean Langley <slangley@juniper.net>
2018-08-13 16:04:47 -0600

    initial commit

--- temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for directory in roles/*; do
-    for taskfolder in "$directory/scripts"; do
-        for mainfile in "$taskfolder/show.py"; do
-             sudo rmate "$mainfile"
-        done
-    done
-done
-    
