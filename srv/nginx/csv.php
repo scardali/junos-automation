936a733277ba578e25a3f46f0a387206c7338c3f
slangley <slangley@juniper.net>
2018-08-23 14:41:25 -0400

    Writing parsed csv data to file

--- srv/nginx/csv.php
+++ srv/nginx/csv.php
@@ -1,4 +1,15 @@
 <?php 
-echo $_SERVER['REQUEST_URI'];
+$headers = array("switch","junos version");
+$data = array("ex","junos-18");
+
+$task = $_GET['task'];
+$filename = "tasks/$task/parsed-data.csv";
+$csvfile = fopen($filename,'w') or die("Unable to open file.");
+for($i = 0; $i < count($headers)-1; $i++){
+    fwrite($csvfile,$headers[$i]);
+    fwrite($csvfile,",");
+}
+fwrite($csvfile,$headers[count($headers)-1]);
+fwrite($csvfile,"\n")
 
 ?>
