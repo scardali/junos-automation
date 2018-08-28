67cbe04c57b15bcf467df09660230f7de7111a49
Sean Langley <slangley@juniper.net>
2018-08-28 10:26:29 -0600

    Python script allows user to select individual roles in run.txt

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
-
+import subprocess
+import sys,os
 with open('run.txt','r') as f:
     lines = f.read().splitlines()
+cmd = 'sudo docker run --rm -v $PWD:$PWD -w $PWD -it juniper/pyez-ansible ansible-playbook get_show_commands.yml --tags "'
+for tag in lines[:-1]:
+    cmd += tag+","
+cmd += lines[-1]
+cmd += '"'
+os.system(cmd)
+
