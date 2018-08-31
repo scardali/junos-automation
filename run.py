452f23662df7aeccde7349e67fa7c58b518d6db9
Sean Langley <slangley@juniper.net>
2018-08-31 09:50:40 -0600

    Docker no longer necessary, make galaxy command to install Juniper.junos role

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import os
 with open('run.txt','r') as f:
     lines = f.read().splitlines()
-cmd = 'sudo docker run --rm -v $PWD:$PWD -w $PWD -it juniper/pyez-ansible ansible-playbook get_show_commands.yml --tags "'
+cmd = 'ansible-playbook get_show_commands.yml --tags "'
 for tag in lines[:-1]:
     cmd += tag+","
 cmd += lines[-1]
